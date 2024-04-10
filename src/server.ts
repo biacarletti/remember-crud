759077ae8b41bd148fc9fb654cfa651f51f7d80a
biacarletti <bia_carletti@hotmail.com>
2024-04-10 09:54:58 -0300

    criar tabela de memórias

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import { fastify } from 'fastify'
+
 const app = fastify()
 
-app.post('/hello', () => {
-  return 'Hello'
-})
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
 })
